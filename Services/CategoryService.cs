d274d475726f8ab57ea838675e0d30f14aeeef09
Caio Martins <dev.caiomartins@gmail.com>
2021-12-02 18:26:06 +0000

    add: Services folder with CategoryService implementation

---
+++ Services/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using aw_api.Domain.Models;
+using aw_api.Domain.Repositories;
+using aw_api.Domain.Services;
+
+namespace aw_api.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _categoryRepository.ListAsync();
+        }
+    }
+}
