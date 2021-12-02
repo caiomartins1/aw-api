5ac600ecf93a9a2e5772127d6780bdfad41fc535
Caio Martins <dev.caiomartins@gmail.com>
2021-12-02 18:24:09 +0000

    add: CategoriesController

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using aw_api.Domain.Models;
+using aw_api.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aw_api.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            return categories;
+        }
+    }
+}
